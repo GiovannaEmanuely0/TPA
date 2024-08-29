de554f712331096c17c6ba1268105a7605a7c6e3
GiovannaEmanuely0 <giovannaEmma16@gmail.com>
2024-08-28 21:32:04 -0300

    Add files via upload

---
+++ Lista-LaçoDeRepetiçaõ/Tabuada.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Tabuada {
+
+	public static void main(String[] args) {
+		
+	Scanner	ler = new Scanner(System.in);
+	
+	System.out.println("Escreva o número do qual você quer saber a tabuada");
+	int n = ler.nextInt();
+	
+	int i;
+	i = 1;
+		
+	while (i <= 10) {
+			int r = n*i;
+			System.out.println(+r);
+			i = i+1;
+		}
+	}
+
+}
