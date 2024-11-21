b3bfe2b6215ab7b8885e53c01f4bc73ffb16012f
Giovanna Emanuely <giovannaEmma16@gmail.com>
2024-11-20 22:56:11 -0300

    Add files via upload

---
+++ ListaVetores/BuscaVetor.java
@@ -0,0 +1,34 @@
+package listaComplementarVetor;
+import java.util.Scanner;
+public class BuscaVetor {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		final int TAM = 5;
+		int a[] = new int [TAM];
+		int x, igual=0;
+		
+		for (int i=0;i<TAM;i++) {
+			System.out.println("Insira um valor:");
+			a[i] = in.nextInt();
+		}
+		System.out.println("Insira um valor de busca:");
+		x = in.nextInt();
+		
+		for(int i = 0; i<TAM; i++) {
+			if(x==a[i]) {
+				 igual++;
+				 break;
+			}
+		}
+		if(igual == 1) {
+			System.out.println("O valor "+x+" esta armazenado em A[]");
+		}else {
+			System.out.println("O valor "+x+" não esta armazenado em A[]");
+		}
+		
+		in.close();
+	}
+
+}
